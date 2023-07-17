3128e52247bdf16870b139729c8307599b031484
Khushiarora18 <91714234+Khushiarora18@users.noreply.github.com>
2023-07-18 00:54:52 +0530

    added apiKeys.js

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "e092b2dd5d51013c3152f72f4a601a60",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
